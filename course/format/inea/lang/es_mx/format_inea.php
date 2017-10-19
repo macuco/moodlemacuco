b3cd03ad1987eaaacb09d7c7a385429421a7b7c8
Juan Manuel - MACUCO <juan.manuel.mp8@gmail.com>
2017-10-18 22:34:08 -0500

    Se agrega el nuevo tema, nuevo formato y se actualiza el mod INEA

---
+++ course/format/inea/lang/es_mx/format_inea.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_topics', language 'es_mx', branch 'MOODLE_33_STABLE'
+ *
+ * @package   format_topics
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addsections'] = 'Añadir formato inea ';
+$string['currentsection'] = 'Este formato';
+$string['deletesection'] = 'Eliminar formato inea';
+$string['editsection'] = 'Editar formato inea';
+$string['editsectionname'] = 'Editar nombre del formato';
+$string['hidefromothers'] = 'Ocultar formato';
+$string['newsectionname'] = 'Nuevo nombre para formato inea {$a}';
+$string['page-course-view-topics'] = 'Cualquier página principal de curso en formato inea';
+$string['page-course-view-topics-x'] = 'Cualquier página de curso en formato inea';
+$string['pluginname'] = 'Formato INEA';
+$string['section0name'] = 'General';
+$string['sectionname'] = 'Contenido';
+$string['showfromothers'] = 'Mostrar formato';
